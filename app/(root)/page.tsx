cdebbf9a786927d88d75076633b61162ea894001
fatima-mousavi224 <fatimamousavi224@gmail.com>
2026-04-18 12:20:02 +0430

    Implment Home filters

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,70 +1,92 @@
+import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import Link from "next/link";
-import { title } from "process";
-  const questions = [
-    {
-      _id: "1",
-      title: "How to laern react?",
-      description: "I am new to react and I want to learn it. Can you help me?",
-      tags: [
-        { _id: "1", name: "reactjs" },
-        { _id: "2", name: "javascript" },
-      ],
-      author: { _id: "1", name: "Sajjad" },
-      upvotes: 10,
-      answers: 5,
-      view: 100,
-      createdAt: new Date(),
-    },
-    {
-      _id: "2",
-      title: "How to laern next?",
-      description: "I am new to next and I want to learn it. Can you help me?",
-      tags: [
-        { _id: "3", name: "nextjs" },
-        { _id: "4", name: "javascript" },
-      ],
-      author: { _id: "1", name: "Sajjad" },
-      upvotes: 10,
-      answers: 5,
-      view: 100,
-      createdAt: new Date(),
 
-    },
-    {
-      _id: "3",
-      title: "How to laern node?",
-      description: "I am new to node and I want to learn it. Can you help me?",
-      tags: [
-        { _id: "5", name: "nodejs" },
-        { _id: "6", name: "javascript" },
-      ],
-      author: { _id: "1", name: "Sajjad" },
-      upvotes: 10,
-      answers: 5,
-      view: 100,
-      createdAt: new Date(),
-    },
-  ];
+const questions = [
+  {
+    _id: "1",
+    title: "How to laern react?",
+    description: "I am new to react and I want to learn it. Can you help me?",
+    tags: [
+      { _id: "1", name: "reactjs" },
+      { _id: "2", name: "javascript" },
+    ],
+    author: { _id: "1", name: "Sajjad" },
+    upvotes: 10,
+    answers: 5,
+    view: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "How to laern next?",
+    description: "I am new to next and I want to learn it. Can you help me?",
+    tags: [
+      { _id: "3", name: "nextjs" },
+      { _id: "4", name: "javascript" },
+    ],
+    author: { _id: "1", name: "Sajjad" },
+    upvotes: 10,
+    answers: 5,
+    view: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "3",
+    title: "How to laern node?",
+    description: "I am new to node and I want to learn it. Can you help me?",
+    tags: [
+      { _id: "5", name: "nodejs" },
+      { _id: "6", name: "javascript" },
+    ],
+    author: { _id: "1", name: "Sajjad" },
+    upvotes: 10,
+    answers: 5,
+    view: 100,
+    createdAt: new Date(),
+  },
+];
 
 interface SearchPrams {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+  searchParams: { query?: string | string[]; filter?: string | string[] };
 }
 
-export default async function Home({searchParams} : SearchPrams) {
-  const { query = "" } = await searchParams;
+export default function Home({ searchParams }: SearchPrams) {
+  const { query = "", filter = "" } = searchParams;
 
-  const normalizedQuery = typeof query === "string"
-    ? query
-    : Array.isArray(query)
-    ? query.join(" ")
-    : "";
+  const normalizedQuery =
+    typeof query === "string"
+      ? query
+      : Array.isArray(query)
+        ? query.join(" ")
+        : "";
 
-  const fillteredQuestions = questions.filter((question) =>
-    question.title.toLowerCase().includes(normalizedQuery.toLowerCase())
-  );
+  const normalizedFilter =
+    typeof filter === "string"
+      ? filter
+      : Array.isArray(filter)
+        ? filter.join(" ")
+        : "";
+
+  const filteredQuestions = questions.filter((question) => {
+    const queryMatch =
+      !normalizedQuery ||
+      question.title.toLowerCase().includes(normalizedQuery.toLowerCase()) ||
+      question.description
+        .toLowerCase()
+        .includes(normalizedQuery.toLowerCase());
+
+    const filterMatch =
+      !normalizedFilter ||
+      question.tags.some((tag) =>
+        tag.name.toLowerCase().includes(normalizedFilter.toLowerCase()),
+      );
+
+    return queryMatch && filterMatch;
+  });
+
   return (
     <>
       <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center ">
@@ -84,9 +106,9 @@ export default async function Home({searchParams} : SearchPrams) {
           route="/"
         />
       </section>
-      Home Filter
+      <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6 ">
-        {fillteredQuestions.map((question) => (
+        {filteredQuestions.map((question) => (
           <h1 key={question._id}>{question.title}</h1>
         ))}
       </div>
