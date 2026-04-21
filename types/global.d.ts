427a1cdaa964aaaff08f9e932ddc3a188075a6fc
fatima-mousavi224 <fatimamousavi224@gmail.com>
2026-04-21 17:41:42 +0430

    Implment create & Read User Routes

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 interface Tag {
     _id: string;
     name: string;
@@ -18,3 +20,18 @@ interface Question {
     answers: number;
     views: number;
 }
+
+type ActionResponse<T = null> = {
+    success: boolean;
+    data?: T;
+    error?: {
+        message: string,
+        details: Record<string, string[]>;
+    },
+    status?: number;
+}
+
+type SuccessResponse<T = null> = ActionResponse<T> & {success: true};
+type ErrorResponse = ActionResponse <undefined> & {success: false}
+type ApiErrorResponse = NextResponse<ErrorResponse>;
+type ApiResponse<T = null> = NextResponse<SuccessResponse<T> | ErrorResponse>;
